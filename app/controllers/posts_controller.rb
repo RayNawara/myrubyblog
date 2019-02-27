93f9a9db9d7e88ac40e9d6a6483f46c44ee49cb6
Raymond J. Nawara <rjnawara@gmail.com>
2019-02-27 10:33:28 -0500

    Database refresh

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,30 @@
+class PostsController < ApplicationController
+    def index
+        @content1 = 'This is sample text for our awesome new Ruby blog'
+        @content2 = 'This is some more sample text for our awesome new Ruby blog'
+    end
+
+    def new
+
+    end
+
+    def create
+
+    end
+    
+    def edit
+
+    end
+    
+    def update
+
+    end
+    
+    def show
+
+    end
+    
+    def destroy
+
+    end
+end
